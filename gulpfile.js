735028a9f697f9d4d20bf052af21395840649a6b
NeoZit <fedotov2189@mail.ru>
2021-07-09 12:40:33 +0300

    added promo and partners

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,40 @@
 const { src, dest, watch, parallel, series } = require('gulp');
-const scss = require('gulp-sass');
+const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
-const browserSync = require('browser-sync').create();
 
 function browsersync() {
     browserSync.init({
         server: {
-            baseDir: "app/"
-        },
-        notify: false
+            baseDir: 'app/'
+        }
     });
 }
 
-function styles() {
-    return src('app/scss/style.scss')
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 versions'],
-            grid: true
-        }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+function cleanDist() {
+    return del('dist')
+}
+
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]
+        ))
+        .pipe(dest('dist/images'))
 }
 
 function scripts() {
@@ -42,47 +51,42 @@ function scripts() {
         .pipe(browserSync.stream())
 }
 
-function images() {
-    return src('app/images/**/*.*')
-        .pipe(imagemin([
-            imagemin.gifsicle({ interlaced: true }),
-            imagemin.mozjpeg({ quality: 75, progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({
-                plugins: [
-                    { removeViewBox: true },
-                    { cleanupIDs: false }
-                ]
-            })
-        ]))
-        .pipe(dest('dist/images'))
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
 }
 
 function build() {
     return src([
-            'app/**/*.html',
             'app/css/style.min.css',
-            'app/js/main.min.js'
+            'app/fonts/**/*',
+            'app/js/main.min.js',
+            'app/*.html'
         ], { base: 'app' })
         .pipe(dest('dist'))
 }
 
-function cleanDist() {
-    return del('dist')
-}
-
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/.js', '!app/js/main.min.js'], scripts);
-    watch(['app/**/*.html']).on('change', browserSync.reload);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
-exports.scripts = scripts;
-exports.browsersync = browsersync;
 exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
 exports.images = images;
 exports.cleanDist = cleanDist;
-exports.build = series(cleanDist, images, build);
 
+
+exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
